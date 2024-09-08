bfc5e9adc82f087384f34e33abf0d3d8b85c820c
Ananda krishnan N <anandakrishnanips2001@gmail.com>
2024-09-08 09:49:40 +0530

    comit

--- ML_project_streamlit/ML_6_wine_quality_prediction/ml6_app.py
+++ ML_project_streamlit/ML_6_wine_quality_prediction/ml6_app.py
@@ -1,3 +1,10 @@
-from ml6_show_page import show_page
+import streamlit as st
+from ml6_show_page import show_explore_page
+from ml6_predict_page import show_predicted_page
 
-show_page()
+page = st.sidebar.selectbox("Data About Wine Qualities Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predicted_page()
+else:
+    show_explore_page()
--- ML_project_streamlit/ML_6_wine_quality_prediction/ml6_show_page.py
+++ ML_project_streamlit/ML_6_wine_quality_prediction/ml6_show_page.py
@@ -8,10 +8,10 @@ import streamlit as st
 
 wine_dataset = pd.read_csv('d:\qtree projeccts\csv and excel files\machine learning project\winequality-red.csv')
 
-def show_page():
+def show_explore_page():
 
     st.title("Explore wine qualities")
-    st.success("Quality if Wine")
+    st.success("Quality of Wine")
     fig, ax = plt.subplots()
     # number of values for each quality
     sns.catplot(x='quality', data= wine_dataset, kind='count', ax=ax)
@@ -20,17 +20,20 @@ def show_page():
 
     # volatile acidity vs quality
     plot = plt.figure(figsize=(5,5))
-    st.bar_chartx='quality', y='volatile acidity', data=wine_dataset)
+    st.bar_chart(x='quality', y='volatile acidity', data=wine_dataset)
 
 
     # citric acid vs quality
     plot = plt.figure(figsize=(5,5))
-    sns.bar_chart(x='quality', y='citric acid', data=wine_dataset)
+    st.bar_chart(x='quality', y='citric acid', data=wine_dataset)
 
     correlation = wine_dataset.corr()
 
     # cunstructing a heat map to understand the correlation between the columns
-    fig1, ax1 = plt.subplots()
+    fig1, ax = plt.subplots()
     plt.figure(figsize=(10,10))
-    sns.heatmap(correlation, cbar=True, square=True, fmt='.1f', annot=True, annot_kws={'size':8}, cmap='Blues', ax1=ax1)
+    sns.heatmap(correlation, cbar=True, square=True, fmt='.1f', annot=True, annot_kws={'size':8}, cmap='Blues', ax=ax)
     st.pyplot(fig1)
+
+    #plt.figure(figsize=(10,10))
+    #sns.heatmap(correlation, cbar=True, square=True, fmt='.1f', annot=True, annot_kws={'size':8}, cmap='Blues')
